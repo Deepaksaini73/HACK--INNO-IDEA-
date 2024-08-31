16bb4206307d6b78a5a87c35100265c8f720e991
DeepjyotiRay-2005 <raydeepjyoti709@gmail.com>
2024-08-31 18:59:07 +0530

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("#signup-btn").addEventListener("click", function() {
+    window.location.href = "signup.html";
+})
+
+document.getElementById("nxt-btn").addEventListener("click", function() {
+    window.location.href = "main.html";
+})
