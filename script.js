58f37faf24821bee339327a866bcd0ba09ce5b98
Deepjyoti Ray <145376141+DeepjyotiRay-2005@users.noreply.github.com>
2024-08-31 22:38:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.querySelector("#signup-btn").addEventListener("click", function() {
     window.location.href = "signup.html";
 })
-
-document.getElementById("nxt-btn").addEventListener("click", function() {
-    window.location.href = "main.html";
-})
